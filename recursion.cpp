2964533791016578957cb45f11d774e9d88c2523
sachin2dehury <sachindehury2015@gmail.com>
2021-06-05 15:46:58 +0530

    Completed Recursion

--- recursion.cpp
+++ recursion.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -29,6 +26,17 @@ int kThSymbol(int n,int k);
 
 void towerOfHanoi(int n,int s,int d,int h);
 
+void printSubSet(string ip,string op);
+void printSubSequence(string ip,string op);
+void printSubString(string ip,string op);
+
+void printPermutationWithSpace(string ip,string op);
+void printPermutationWithCase(string ip,string op);
+void printPermutationWithCase(string ip,string op);
+
+void printBinaryNoPrefix(int n,int o,int z,string op);
+
+int josephusProblem(int n,int k);
 
 int treeHeight(node* root) {
 	if(root == nullptr) {
@@ -138,6 +146,100 @@ void towerOfHanoi(int n,int s=1,int d=3,int h=2) {
 	towerOfHanoi(n-1,h,d,s);
 }
 
+void printSubSet(string ip,string op="") {
+	if(ip.length()==0) {
+		cout<<op<<endl;
+		return;
+	}
+	string op1= op;
+	string op2= op+ip[0];
+	printSubSet(ip.substr(1),op1);
+	printSubSet(ip.substr(1),op2);
+}
+
+void printSubSequence(string ip,string op="") {
+	if(ip.length()==0) {
+		if(op.length()>0) {
+			cout<<op<<endl;
+		}
+		return;
+	}
+	string op1= op;
+	string op2= op+ip[0];
+	printSubSequence(ip.substr(1),op1);
+	printSubSequence(ip.substr(1),op2);
+}
+
+void printSubString(string ip,string op="") {
+	if(ip.length()==0) {
+		return;
+	}
+	string op1 = "";
+	string op2 = op+ip[0];
+	cout<<op2<<endl;
+	printSubString(ip.substr(1),op2);
+	printSubString(ip.substr(1),op1);
+}
+
+void printPermutationWithSpace(string ip,string op="") {
+	if(ip.length()==0) {
+		cout<<op<<endl;
+		return;
+	}
+	string op1= op+ip[0];
+	printPermutationWithSpace(ip.substr(1),op1);
+	if(op.length()>0) {
+		string op2= op+' '+ip[0];
+		printPermutationWithSpace(ip.substr(1),op2);
+	}
+}
+
+void printPermutationWithCase(string ip,string op="") {
+	if(ip.length()==0) {
+		cout<<op<<endl;
+		return ;
+	}
+	string op1= op+ ip[0];
+	string op2= op + (char)toupper(ip[0]);
+	printPermutationWithCase(ip.substr(1),op1);
+	printPermutationWithCase(ip.substr(1),op2);
+}
+
+void printCombinationOfParenthesis(int o,int c,string op="") {
+	if(o==0 && c==0) {
+		cout<<op<<endl;
+		return;
+	}
+	string op1 = op+'(';
+	string op2 = op+')';
+	if(o>0) {
+		printCombinationOfParenthesis(o-1,c,op1);
+	}
+	if(c>o) {
+		printCombinationOfParenthesis(o,c-1,op2);
+	}
+}
+
+void printBinaryNoPrefix(int n,int o=0,int z=0,string op="") {
+	if(n==0){
+		cout<<op<<endl;
+		return;
+	}
+	string op1= op+'1';
+	string op2= op+'0';
+	printBinaryNoPrefix(n-1,o+1,z,op1);
+	if(o>z) {
+		printBinaryNoPrefix(n-1,o,z+1,op2);
+	}
+}
+
+int josephusProblem(int n,int k) {
+	if(n==1) {
+		return 1;
+	}
+	return (josephusProblem(n-1,k)+k-1)%n+ 1;
+}
+
 int main() {
 	
 	vector<int> v = {4,3,2,1};
@@ -145,8 +247,6 @@ int main() {
 	// for(int i : v){
 	// 	cout<<i<<" ";
 	// }
-
-	cout<<endl;
 
 	stack<int> s;
 	s.push(5);
@@ -181,5 +281,27 @@ int main() {
 	// towerOfHanoi(4);
 
 
+	// printSubSet("abcd");
 
+	// printSubSequence("ab");
+
+	// sort(str.begin(),str.end());
+
+	// for(string i : str) {
+	// 	cout<<i<<endl;
+	// }
+
+	// printSubString("abc");
+
+	// printPermutationWithSpace("abc");
+
+	// printPermutationWithCase("abc");
+
+	// printPermutationWithCase("a1b2c");
+
+	// printCombinationOfParenthesis(3,3);
+
+	// printBinaryNoPrefix(5);
+
+	// cout<<josephusProblem(5,2);
 }
